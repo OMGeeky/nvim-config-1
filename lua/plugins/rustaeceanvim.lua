b4f99d775d7f69d5a3b165d3ae42afc47f615d4c
Frederik <fb@omgeeky.de>
2023-12-26 12:07:29 +0100

    fix multiple rust-analyzer by disabling one

--- lua/plugins/rustaeceanvim.lua
+++ lua/plugins/rustaeceanvim.lua
@@ -4,4 +4,14 @@ return {
     version = "^3", -- Recommended
     ft = { "rust" },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+    },
+  },
 }
